1445881a72605d510fc17bf9a103eaa9fd126266
ag-su <eunsugim815@gmail.com>
2023-02-20 11:09:34 +0900

    😻 change csv file to parquet file link

--- stockAI/dataLoad.py
+++ stockAI/dataLoad.py
@@ -4,18 +4,16 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-
-# 종목 불러오기
 def get_tickers(markets:list, date:str): 
-    df = pd.read_csv("https://media.githubusercontent.com/media/stockAI-py/data-test/main/KOR/data_2001_2022.csv")
+    df = pd.read_parquet("https://media.githubusercontent.com/media/stockAI-py/stock_data/main/v1_krx_2001_2022/krx_20010101_20221231.parquet")
     df_market = df[df['Market'].isin(markets)]
     lst_tickers = df_market.loc[df_market['Date'] >= f'{date}-01-01', 'Code'].unique().tolist() 
     return lst_tickers
 
 
-# 주가 데이터 불러오기
+
 def load_data(date, tickers):
-    df = pd.read_csv("https://media.githubusercontent.com/media/stockAI-py/data-test/main/KOR/data_2001_2022.csv")
+    df = pd.read_parquet("https://media.githubusercontent.com/media/stockAI-py/stock_data/main/v1_krx_2001_2022/krx_20010101_20221231.parquet")
     df_date = df[(df['Date'] >= date[0]) & (df['Date'] <= date[1])]
     df_code = df_date[df_date['Code'].isin(tickers)].reset_index(drop=True)
     
