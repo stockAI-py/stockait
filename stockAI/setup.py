ee1e5de83ec76d61ec55ec4e62f8435db5969fd6
ChaYujin-kr <59563689+ChaYujin-kr@users.noreply.github.com>
2023-03-21 12:08:10 +0900

    Update setup.py

--- stockAI/setup.py
+++ stockAI/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="stockai-py", # Replace with your own username
+    name="stockair", # Replace with your own username
     version="0.0.1",
     author="Eunsu Kim, Sieun Kim, Eunji Cha, Yujin Cha",
     author_email="stockai2023@gmail.com",
