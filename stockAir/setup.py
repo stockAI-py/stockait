969c21bb0a579182423ca8a22f2f245c95df7ae8
ChaYujin-kr <59563689+ChaYujin-kr@users.noreply.github.com>
2023-03-23 23:38:07 +0900

    Update setup.py

--- stockAir/setup.py
+++ stockAir/setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.0.0",    # pypi 에 등록할 version (수정할 때마다 version up을 해줘야 함)
     description="Make your stock investment smarter, join StockAir!",
     author="Eunsu Kim, Sieun Kim, Eunji Cha, Yujin Cha",
-    author_email=""stockai2023@gmail.com"",
+    author_email="stockai2023@gmail.com",
     url="https://github.com/insutance/hey-insutance",
     python_requires=">= 3.8",
     packages=find_packages(),
