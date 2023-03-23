d57fb74068f02882e46a19dd8911500564d1b900
ChaYujin-kr <59563689+ChaYujin-kr@users.noreply.github.com>
2023-03-23 23:35:29 +0900

    Update setup.py

--- stockAir/setup.py
+++ stockAir/setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="stockAir",   # pypi 에 등록할 라이브러리 이름
     version="0.0.0",    # pypi 에 등록할 version (수정할 때마다 version up을 해줘야 함)
-    description="Hey insutance! Who are you?",
-    author="insutance",
-    author_email="insutance@naver.com",
+    description="Make your stock investment smarter, join StockAir!",
+    author="Eunsu Kim, Sieun Kim, Eunji Cha, Yujin Cha",
+    author_email=""stockai2023@gmail.com"",
     url="https://github.com/insutance/hey-insutance",
     python_requires=">= 3.8",
     packages=find_packages(),
